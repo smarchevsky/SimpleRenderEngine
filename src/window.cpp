0ff45fbb5842d30afaf7b16174bebf9277b10b1a
smarchevsky <staseg1987@gmail.com>
2022-05-14 13:28:13 +0300

    Gamepad support

--- src/window.cpp
+++ src/window.cpp
@@ -8,6 +8,11 @@
 #include <iostream>
 #include <string>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+
+//#define LOG_GAME_CONTROLLERS
+
 static int SDLCALL my_event_filter(void* userdata, SDL_Event* event)
 {
     Window* p_window = (Window*)userdata;
@@ -26,6 +31,67 @@ static int SDLCALL my_event_filter(void* userdata, SDL_Event* event)
             return 1;
         }
     }
+
+    if (event->type == SDL_JOYAXISMOTION) {
+        SDL_Joystick* joystick = SDL_JoystickFromInstanceID(event->jaxis.which);
+        auto it = p_window->m_joysticks.find(joystick);
+        if (it != p_window->m_joysticks.end()) {
+
+            GameControllerData& data = it->second; // TODO redo using gamepad mapping
+            bool evaluated = true;
+            switch (event->jaxis.axis) {
+            case 0: // left
+                data.slx_value = event->jaxis.value;
+                break;
+            case 1:
+                data.sly_value = event->jaxis.value;
+                break;
+            case 2:
+                data.tl_value = event->jaxis.value;
+                break;
+            case 3: // right
+                data.srx_value = event->jaxis.value;
+                break;
+            case 4:
+                data.sry_value = event->jaxis.value;
+                break;
+            case 5:
+                data.tr_value = event->jaxis.value;
+                break;
+            case 6:
+                // left and right triggers connected, do nothing
+                break;
+
+            default:
+                evaluated = false;
+            }
+            evaluated = evaluated; // fuck never read warning
+#ifdef LOG_GAME_CONTROLLERS
+            std::cout << "Controller: " << SDL_JoystickName(joystick);
+            if (evaluated)
+                std::cout
+                    << "  Axis: " << (int)event->jaxis.axis
+                    << "  StickLeft: " << glm::to_string(glm::ivec2(data.slx_value, data.sly_value))
+                    << "  StickRight: " << glm::to_string(glm::ivec2(data.srx_value, data.sry_value))
+                    << "  TriggerLeft: " << data.tl_value
+                    << "  TriggerRight: " << data.tr_value
+                    << std::endl;
+            else {
+                std::cout
+                    << "\t Undefined axis: " << (int)event->jaxis.axis
+                    << "\t Value: " << event->jaxis.value
+                    << std::endl;
+            }
+#endif
+        } else {
+            std::cout << "This joystick not listed before..." << std::endl;
+        }
+    }
+
+    if (event->type != SDL_POLLSENTINEL) {
+        // std::cout << "EventType: " << (int)event->type << "Axis: " << (int)event->caxis.axis << std::endl;
+        // std::cout << event->type << std::endl;
+    }
     return 0;
 }
 
@@ -33,8 +99,8 @@ Window::Window(int width /*= 800*/, int height /*= 600*/)
     : m_width(width)
     , m_height(height)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        // std::cerr << "SDL2 video subsystem couldn't be initialized. Error: " << SDL_GetError() << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0) {
+        std::cerr << "SDL2 video subsystem couldn't be initialized. Error: " << SDL_GetError() << std::endl;
         exit(1);
     }
 
@@ -50,7 +116,7 @@ Window::Window(int width /*= 800*/, int height /*= 600*/)
         std::cerr << "SDL2 Renderer couldn't be created. Error: " << SDL_GetError() << std::endl;
         exit(1);
     }
-
+    initGamepad();
     SDL_GL_MakeCurrent(m_window, gl_context);
     SDL_SetEventFilter(my_event_filter, this);
 
@@ -58,6 +124,38 @@ Window::Window(int width /*= 800*/, int height /*= 600*/)
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glClearColor(0.08, 0.08, 0.1, 1);
+}
+
+void Window::initGamepad()
+{
+    int nJoysticks = SDL_NumJoysticks();
+    int nGameControllers = 0;
+    std::string msg;
+    for (int i_joystick = 0; i_joystick < nJoysticks; i_joystick++) {
+        SDL_Joystick* joystick = SDL_JoystickOpen(i_joystick);
+        SDL_GameController* controller = SDL_GameControllerOpen(i_joystick);
+
+        if (joystick)
+            std::cout << SDL_JoystickName(joystick) << std::endl;
+
+        if (controller) {
+            std::cout << SDL_GameControllerName(controller) << std::endl;
+            // std::cout << SDL_GameControllerMapping(controller) << std::endl;
+        }
+
+        m_joysticks.insert({ joystick, {} });
+        // m_gameControllers.push_back(controller);
+
+        // const char* name = SDL_GameControllerName(controller);
+        //  const char* mapping = SDL_GameControllerMapping(controller);
+
+        //        msg += std::string("Name: ") + name + "  \n";
+        //        if (SDL_IsGameController(i_joystick)) {
+        //            nGameControllers++;
+    }
+    //}
+    // std::cout << msg << std::endl;
+    //  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Controllers", msg.c_str(), nullptr);
 }
 
 bool Window::update()
@@ -68,7 +166,6 @@ bool Window::update()
     SDL_SetWindowTitle(m_window, std::to_string(1 / m_deltaTime).c_str());
 
     SDL_Event event;
-
     while (SDL_PollEvent(&event)) { // poll until all events are handled!
         // decide what to do with this event.
     }
@@ -90,4 +187,9 @@ Window::~Window()
     SDL_DestroyWindow(m_window);
 
     SDL_Quit();
+}
+
+void Window::ErrorMsg(const char* title, const char* msg)
+{
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, msg, m_window);
 }
--- src/window.h
+++ src/window.h
@@ -2,32 +2,43 @@
 #define WINDOW_H
 
 #include "keymap.h"
-
 #include <glm/glm.hpp>
 
 typedef void* SDL_GLContext;
+struct _SDL_Joystick;
+
+struct GameControllerData {
+    int16_t slx_value {}, sly_value {}, srx_value {}, sry_value {};
+    int16_t tl_value {}, tr_value {};
+};
 
 class Window {
 public:
     Window(int width = 800, int height = 600);
-    bool update();
-    float getDeltaTime() { return m_deltaTime; }
-    ~Window();
+    void initGamepad();
 
-    glm::vec2 getMousePos();
+    bool update();
+
+    float getDeltaTime() { return m_deltaTime; }
+    // glm::vec2 getMousePos();
 
     void closeWindow() { m_isRendering = false; }
     KeyMap& getKeyMap() { return m_keyMap; }
+    ~Window();
+
+    void ErrorMsg(const char* title, const char* msg);
+    std::unordered_map<_SDL_Joystick*, GameControllerData> m_joysticks;
 
 private:
     KeyMap m_keyMap;
     struct SDL_Window* m_window;
     struct SDL_Renderer* m_renderer;
+
     SDL_GLContext gl_context;
     int m_width, m_height;
-    bool m_isRendering = true;
 
-    float m_deltaTime {};
     uint64_t NOW {}, LAST {};
+    float m_deltaTime {};
+    bool m_isRendering = true;
 };
 #endif // WINDOW_H
